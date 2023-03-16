ddbd8a6886a27888a31ad29b4f7b62b799e5c969
Andreas Steinkellner <asteinkellner1@gmail.com>
2023-03-17 00:27:00 +0100

    config: add numpad layer, remove music layer

--- keyboards/preonic/keymaps/NarrowCode/keymap.c
+++ keyboards/preonic/keymaps/NarrowCode/keymap.c
@@ -24,7 +24,7 @@ enum preonic_layers {
   _LOWER,
   _RAISE,
   _ADJUST,
-  _MUSIC
+  _NUMP
 };
 
 enum preonic_keycodes {
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,          KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   KC_ESC,  KC_A,    KC_S,    KC_D,          KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,          KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  KC_LCTL, KC_LGUI, KC_LALT, OSL(_ADJUST),  LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LCTL, KC_LGUI, KC_LALT, MO(_NUMP),     LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak
@@ -115,11 +115,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_NUM_LOCK,  KC_P7,   KC_P8,   KC_P9,   KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_BSPC,
+  _______,      KC_P4,   KC_P5,   KC_P6,   KC_DLR,  KC_PERC, KC_CIRC, _______, KC_UP,   _______, KC_PGUP,  KC_DEL,
+  _______,      KC_P1,   KC_P2,   KC_P3,   KC_F4,   KC_F5,   KC_F6,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,  KC_PIPE,
+  _______,      KC_P0,   KC_P0,   KC_PCMM, KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, KC_MPLY,
+  _______,      _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU,  KC_MNXT
 ),
 
 /* Raise
@@ -137,10 +137,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_preonic_grid(
   KC_GRV,  KC_F1,       KC_F2,      KC_F3,      KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
-  KC_GRV,  KC_MS_BTN1,  KC_MS_UP,   KC_MS_BTN2, KC_WH_U,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_DEL,  KC_MS_LEFT,  KC_MS_DOWN, KC_MS_RIGHT,KC_WH_D,    KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  _______, KC_F7,       KC_F8,      KC_F9,      KC_F10,     KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-  _______, _______,     _______,    _______,    _______,    _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_GRV,  KC_MS_BTN1,  KC_MS_UP,   KC_MS_BTN2, KC_WH_U,    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_LBRC, KC_RBRC,
+  KC_DEL,  KC_MS_LEFT,  KC_MS_DOWN, KC_MS_RIGHT, KC_WH_D,   _______, KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  _______, KC_WH_D,     KC_WH_U,    KC_F9,      KC_F10,     KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_MPLY,
+  _______, _______,     _______,    _______,    _______,    _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
 ),
 
 /* Adjust (Lower + Raise)
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //  ESC   1                        2                           3                       4                       5                       6                       7           8           9           0           DEL
 [_ADJUST] = LAYOUT_preonic_grid(
   _______, LCTL(LALT(LGUI(KC_1))), LCTL(LALT(LGUI(KC_2))),      LCTL(LALT(LGUI(KC_3))), LCTL(LALT(LGUI(KC_4))), LCTL(LALT(LGUI(KC_5))), LCTL(LALT(LGUI(KC_6))), KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
-  _______, _______,                 _______,                    _______,                _______,                _______,                _______,                RALT(KC_U), _______,    RALT(KC_O), _______,    TO(_MUSIC),
+  _______, _______,                 _______,                    _______,                _______,                _______,                _______,                RALT(KC_U), _______,    RALT(KC_O), _______,    _______,
   _______, RALT(KC_A),              RALT(KC_S),                 AU_ON,                  AU_OFF,                 AG_NORM,                AG_SWAP,                QWERTY,     _______,    _______,    _______,    _______,
   _______, AU_PREV,                 AU_NEXT,                    MU_ON,                  MU_OFF,                 MI_ON,                  MI_OFF,                 _______,    _______,    _______,    _______,    _______,
   _______, _______,                 _______,                    _______,                _______,                _______,                _______,                _______,    _______,    _______,    QK_BOOT,    DB_TOGG
@@ -174,14 +174,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  _______, AU_PREV,     AU_NEXT,    MU_ON,      MU_OFF,     MI_ON,      MI_OFF,     _______,    _______,    _______,    _______,    _______,
 //  _______, _______,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
 //)
+//
 
-// Special layer for default ADJUSTMENT config.
-[_MUSIC] = LAYOUT_preonic_grid(
-  KC_F1,   KC_F2,       KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
-  _______, QK_BOOT,     DB_TOGG,    _______,    _______,    _______,    _______,    RALT(KC_U), _______,    _______,    _______,    TO(_QWERTY),
-  _______, RALT(KC_A),  MU_NEXT,    AU_ON,      AU_OFF,     AG_NORM,    AG_SWAP,    QWERTY,     COLEMAK,    DVORAK,     _______,    _______,
-  _______, AU_PREV,     AU_NEXT,    MU_ON,      MU_OFF,     MI_ON,      MI_OFF,     _______,    _______,    _______,    _______,    _______,
-  _______, _______,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
+
+// Numpads are awesome.
+[_NUMP] = LAYOUT_preonic_grid(
+  _______,    _______,     _______,    _______,    _______,    _______,    _______,    KC_P7,      KC_P8,      KC_P9,      _______,    _______,
+  _______,    _______,     _______,    _______,    _______,    _______,    _______,    KC_P4,      KC_P5,      KC_P6,      _______,    _______,
+  _______,    _______,     _______,    _______,    _______,    _______,    _______,    KC_P1,      KC_P2,      KC_P3,      _______,    _______,
+  _______,    _______,     _______,    _______,    _______,    _______,    _______,    KC_P0,      KC_P0,      KC_PCMM,    _______,    _______,
+  _______,    _______,     _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
 )
 
 
